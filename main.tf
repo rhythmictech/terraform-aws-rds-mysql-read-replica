43f9c4e6c4db7b5a5138d69132ad4e3e2c499477
Kevin <laxmaniac@gmail.com>
2021-09-16 13:40:50 -0400

    Setup module

--- main.tf
+++ main.tf
@@ -1,16 +1,51 @@
-
-module "tags" {
-  source  = "rhythmictech/tags/terraform"
-  version = "~> 1.1.0"
-
-  enforce_case = "UPPER"
-  names        = [var.name]
-  tags         = var.tags
+locals {
+  engine                    = "mysql"
+  final_snapshot_identifier = var.final_snapshot_identifier == null ? "${var.name}-final-snapshot" : var.final_snapshot_identifier
+  parameter_group_name      = length(var.parameters) > 0 ? aws_db_parameter_group.this[0].name : null
 }
 
-locals {
-  # tflint-ignore: terraform_unused_declarations
-  name = module.tags.name
-  # tflint-ignore: terraform_unused_declarations
-  tags = module.tags.tags_no_name
+resource "aws_db_parameter_group" "this" {
+  count = length(var.parameters) > 0 ? 1 : 0
+
+  name_prefix = "${var.name}-param"
+
+  family = "${local.engine}${var.engine_version}"
+
+  dynamic "parameter" {
+    iterator = each
+    for_each = var.parameters
+
+    content {
+      name         = each.value.name
+      apply_method = each.value.apply_method
+      value        = each.value.value
+    }
+  }
+}
+
+resource "aws_db_instance" "this" {
+  backup_retention_period             = var.backup_retention_period
+  copy_tags_to_snapshot               = var.copy_tags_to_snapshot
+  deletion_protection                 = var.enable_deletion_protection
+  enabled_cloudwatch_logs_exports     = var.cloudwatch_log_exports
+  iam_database_authentication_enabled = var.iam_database_authentication_enabled
+  identifier_prefix                   = var.identifier_prefix
+  instance_class                      = var.instance_class
+  monitoring_interval                 = var.monitoring_interval
+  monitoring_role_arn                 = var.monitoring_role_arn
+  multi_az                            = var.multi_az
+  parameter_group_name                = local.parameter_group_name
+  performance_insights_enabled        = var.performance_insights_enabled
+  port                                = var.port
+  replicate_source_db                 = var.replicate_source_db_id
+  storage_encrypted                   = var.storage_encrypted
+  storage_type                        = var.storage_type
+  final_snapshot_identifier           = local.final_snapshot_identifier
+  skip_final_snapshot                 = var.skip_final_snapshot
+
+  tags = merge(var.tags,
+    {
+      "Name" = var.name
+    }
+  )
 }
--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,20 @@
+output "address" {
+  description = "RDS database address"
+  value       = aws_db_instance.this.address
+}
 
-output "tags_module" {
-  description = "Tags Module in it's entirety"
-  value       = module.tags
+output "instance_connection_info" {
+  description = "Object containing connection info"
+  value = {
+    address  = aws_db_instance.this.address
+    endpoint = aws_db_instance.this.endpoint
+    id       = aws_db_instance.this.id
+    port     = aws_db_instance.this.port
+    username = aws_db_instance.this.username
+  }
+}
+
+output "instance_id" {
+  description = "Instance ID of RDS DB"
+  value       = aws_db_instance.this.id
 }
